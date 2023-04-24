300134b1382d5d06da1731fea6c6fc88a839a96b
araujo <araujoyadir@gmail.com>
2023-04-24 21:17:44 +0200

    Initial commit

---
+++ src/com/hedima/presentacion/Ejercicio_1.java
@@ -0,0 +1,10 @@
+package com.hedima.modelo;
+
+public class Ejercicio_1 {
+    // Definir un Set con la clase HashSet(), y añadir los elementos: Buenas noches, 5, 8.5, 5, Calificación.
+    // Mostrar los elementos con el método toString y mostrar de que clase son
+
+    
+
+
+}
